4ca5ed7b594b6561c48a391c20da8b6854e8261d
Dexter Edwards <dexter.edwards@ig.com>
2018-04-11 14:57:43 +0100

    Basic ember no double sets

---
+++ tests/lib/rules/ember-no-double-sets.js
@@ -0,0 +1,86 @@
+/**
+ * @fileoverview Use setProperties over multiple sets
+ * @author Dexter Edwards
+ */
+'use strict';
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+var rule = require('../../../lib/rules/ember-no-double-sets');
+
+var RuleTester = require('eslint').RuleTester;
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run('ember-no-double-sets', rule, {
+
+  valid: [
+    {
+      code: `
+            {
+              set1(this, 'a', b);
+              set1(this, 'c', d);
+            }
+            `
+    },
+    {
+      code: `
+            {
+              set(this, 'a', b);
+            }
+            `
+    },
+    {
+      code: `
+            {
+              object.set(this, 'a', b);
+              object.set(this, 'c', d);
+            }
+            `
+    }
+  ],
+
+  invalid: [
+    {
+      code: `
+            {
+              set(this, 'a', b);
+              set(this, 'c', d);
+            }
+            `,
+      errors: [{
+        message: 'Use setProperties if you need to set mutiple properties'
+      }]
+    },
+    {
+      code: `
+            {
+              {
+                set(this, 'a', b);
+                set(this, 'c', d);
+              }
+            }
+            `,
+      errors: [{
+        message: 'Use setProperties if you need to set mutiple properties'
+      }]
+    },
+    {
+      code: `
+            {
+              set(this, 'a', b);
+              set(this, 'c', d);
+              set(this, 'e', f);
+            }
+            `,
+      errors: [{
+        message: 'Use setProperties if you need to set mutiple properties'
+      }]
+    }
+  ]
+});
